3cd460a1f981ee53c9a3694edcd9cc064aeb1412
minaorangina <wilhelmina.jane@gmail.com>
2015-05-04 08:18:11 +0100

    added object containing roman numerals

--- roman.js
+++ roman.js
@@ -32,6 +32,16 @@ else{
 	matchResult[1] == 'number' ? toRoman() : toNumber();
 }
 
+var roman = {
+			I : 1,
+			V : 5,
+			X : 10,
+			L : 50,
+			C : 100,
+			D : 500,
+			M : 1000
+		};
+
 
 // toNumber
 function toNumber(){
